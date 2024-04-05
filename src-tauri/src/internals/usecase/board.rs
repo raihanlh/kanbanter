a327ae4f19a971a0311b597f04ff6b0f18a50aeb
Raihan Luthfi <raihan.luthfi.h@gmail.com>
2024-04-05 15:41:38 +0700

    implement board usecases

--- src-tauri/src/internals/usecase/board.rs
+++ src-tauri/src/internals/usecase/board.rs
@@ -5,4 +5,9 @@ use crate::internals::model::board::Board;
 #[async_trait]
 pub trait BoardUsecase {
     async fn create_new_board(&self, board: Board) -> Box<Board>;
+    async fn get_board_by_id(&self, id: i64) -> Box<Board>;
+    async fn get_all_boards(&self) -> Vec<Box<Board>>;
+    async fn update_board_by_id(&self, board: Board) -> Box<Board>;
+    async fn get_highest_board_position(&self, id: i64) ->  i32;
+    async fn delete_board_by_id(&self, id: i64) ->  bool;
 }
--- src-tauri/src/internals/usecase/board_impl.rs
+++ src-tauri/src/internals/usecase/board_impl.rs
@@ -21,4 +21,23 @@ impl BoardUsecase for BoardUsecaseImpl {
     async fn create_new_board(&self, board: Board) -> Box<Board> {
         self.repo.insert(board).await
     }
+
+    async fn get_board_by_id(&self, id: i64) -> Box<Board> {
+        self.repo.get_by_id(id).await
+    }
+
+    async fn get_all_boards(&self) -> Vec<Box<Board>> {
+        self.repo.get_all().await
+    }
+    async fn update_board_by_id(&self, board: Board) -> Box<Board> {
+        self.repo.update(board).await
+    }
+
+    async fn get_highest_board_position(&self, id: i64) ->  i32 {
+        self.repo.get_highest_board_position().await
+    }
+
+    async fn delete_board_by_id(&self, id: i64) ->  bool {
+        self.repo.delete(id).await
+    }
 }
