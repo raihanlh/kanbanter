dc859e67907f18c6af8f4fb88de39f0f34466227
Raihan Luthfi Haryawan <raihan.luthfi.h@gmail.com>
2024-04-23 13:14:36 +0700

    fix dropdown menu

--- src/components/menu/DropdownMenu.tsx
+++ src/components/menu/DropdownMenu.tsx
@@ -1,4 +1,4 @@
-import { FC, Fragment, MouseEventHandler, ReactNode } from "react";
+import { FC, Fragment, MouseEventHandler, ReactNode, useEffect, useRef, useState } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import { classNames } from "@/lib/utils";
@@ -21,16 +21,39 @@ export const DropdownMenu: FC<DropdownMenuProps> = ({
   dropdownItems,
   buttonColor = "white",
 }) => {
+  const buttonRef = useRef(null);
+  const [dropdownPosition, setDropdownPosition] = useState({ top: 0, left: 0 });
+
+  // Function to position the dropdown menu
+  const positionDropdownMenu = () => {
+    if (buttonRef.current) {
+      const buttonRect = buttonRef.current.getBoundingClientRect();
+      setDropdownPosition({ top: buttonRect.bottom, left: buttonRect.left });
+    }
+  };
+
+  // Call the function initially and whenever the window is resized or scrolled
+  useEffect(() => {
+    positionDropdownMenu();
+    console.log(dropdownPosition);
+    window.addEventListener('resize', positionDropdownMenu);
+    window.addEventListener('scroll', positionDropdownMenu);
+    return () => {
+      window.removeEventListener('resize', positionDropdownMenu);
+      window.removeEventListener('scroll', positionDropdownMenu);
+    };
+  }, []);
+  
   const buttonClass = `inline-flex w-full justify-center gap-x-1.5 rounded-md bg-transparent pl-3 px-1 py-2 text-sm font-semibold text-${buttonColor}-900`;
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button className={buttonClass}>
+        <Menu.Button className={buttonClass} ref={buttonRef}
+        onClick={() => {
+          positionDropdownMenu();
+        }}
+        >
           {text}
-          {/* <ChevronDownIcon
-            className="-mr-1 h-5 w-5 text-gray-400"
-            aria-hidden="true"
-          /> */}
         </Menu.Button>
       </div>
 
@@ -43,7 +66,11 @@ export const DropdownMenu: FC<DropdownMenuProps> = ({
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="fixed -translate-x-48 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items 
+        style={{ position: 'fixed', top: dropdownPosition.top, left: dropdownPosition.left }}
+        className="mt-2 w-56 origin-top-right rounded-md bg-white focus:outline-none"
+        // className={`fixed top-${dropdownPosition.top} left-${dropdownPosition.left} tran mt-2 w-56 origin-top-right rounded-md bg-white ring-1 ring-black ring-opacity-5 focus:outline-none`}
+        >
           <div className="py-1">
             {dropdownItems.map((item, index) => (
               <Menu.Item key={index}>
